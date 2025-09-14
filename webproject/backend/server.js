7aee08048b969dd31dfb5621812c3f5e5d2bf65d
Ángel Murciano Baena <a.murciano.2021@alumnos.urjc.es>
2025-09-14 16:29:22 +0200

    Implementation of Node.js and CORS

---
+++ webproject/backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Sirve la carpeta "frontend" como archivos estáticos
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html')); // index.html por defecto, ya le colocaré algo más tarde
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${PORT}`); // Aviso de que el server anda corriendo en la consola
+});
