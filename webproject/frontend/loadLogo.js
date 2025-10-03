fc0c25822a10e1e9d7340344767efd08b92c038f
Ángel Murciano Baena <a.murciano.2021@alumnos.urjc.es>
2025-10-03 22:09:45 +0200

    Addition of wait for logo loading

--- webproject/frontend/loadLogo.js
+++ webproject/frontend/loadLogo.js
@@ -1,15 +1,18 @@
-// Obtener logo
-fetch('images/logo.png')
-// Transformar en blob si se obtuvo bien
-.then(response => {
-    if(!response.ok) throw new Error('No se pudo cargar la imagen correctamente.');
-    return response.blob();
-})
-// Crear elemento img para poner en el header
-.then(blob => {
-    const url = URL.createObjectURL(blob);
-    const img = document.createElement('img');
-    img.src = url;
-    img.alt = 'logo-mercadona';
-    document.getElementById('logo').appendChild(img);
-})
+// Esperar a que la página se estructure (ASÍ ES MEJOR!)
+document.addEventListener('DOMContentLoaded', () => {
+    // Obtener logo
+    fetch('images/logo.png')
+    // Transformar en blob si se obtuvo bien
+    .then(response => {
+        if(!response.ok) throw new Error('No se pudo cargar la imagen correctamente.');
+        return response.blob();
+    })
+    // Crear elemento img para poner en el header
+    .then(blob => {
+        const url = URL.createObjectURL(blob);
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = 'logo-mercadona';
+        document.getElementById('logo').appendChild(img);
+    })
+});
